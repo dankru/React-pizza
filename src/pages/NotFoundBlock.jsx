ff4996b3cb3a08bc6091814afaabb49b6cc19acf
Neutral <pirogovde@mer.ci.nsu.ru>
2022-09-25 23:01:23 +0300

    added Cart Component and NotFoundBlock component, added Routing with Links

---
+++ src/pages/NotFoundBlock.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFoundBlock = () => {
+  return (
+    <div>
+      <h1>
+        <span>Страница не существует</span>
+      </h1>
+    </div>
+  );
+};
+
+export default NotFoundBlock;
