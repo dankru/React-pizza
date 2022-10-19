e9fa93e1352b4b4e5104ba25fcac8e778baeca29
Neutral <pirogovde@mer.ci.nsu.ru>
2022-10-19 13:08:03 +0300

    made app.js function component and used react-redux hooks to store data

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 //axios needed for more complex requests
 import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Header } from './components';
 import { Home, Cart, NotFoundBlock } from './pages';
+import setPizzas from './redux/actions/pizzas';
 
 function App() {
-  const [pizzas, setPizzas] = React.useState([]);
-
+  //const [pizzas, setPizzas] = React.useState([]);
+  const dispatch = useDispatch();
+  const { items } = useSelector(({ pizzas, filters }) => {
+    return {
+      items: pizzas.items,
+      sortBy: filters.sortBy,
+    };
+  });
   React.useEffect(() => {
     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      setPizzas(data.pizzas);
+      dispatch(setPizzas(data.pizzas));
     });
   }, []);
 
@@ -20,7 +28,7 @@ function App() {
       <Header></Header>
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home items={pizzas} />} />
+          <Route path="/" element={<Home items={items} />} />
           <Route path="/not-found" element={<NotFoundBlock />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
--- src/index.js
+++ src/index.js
@@ -7,15 +7,6 @@ import store from './redux/store';
 import { Provider } from 'react-redux';
 import setPizzas from './redux/actions/pizzas';
 
-store.subscribe(() => {
-  console.log('Хранилище изменилось', store.getState());
-});
-
-store.dispatch({
-  type: 'SET_CATEGORY',
-  payload: 2,
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
